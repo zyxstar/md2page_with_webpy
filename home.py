fc03891d9eeaf646a342395fcc5fd2f53311be73
zyxstar <248340019@qq.com>
2015-03-02 11:25:11 +0800

    change github to gitcafe

--- home.py
+++ home.py
@@ -6,7 +6,7 @@ from lib import handlers
 app_root = os.path.dirname(__file__)
 
 handlers.RES_BASE_URL_PATH = "/static"
-handlers.SAMPLE_NOTE_URL = "https://github.com/zyxstar/md_note/raw/master/README.md"
+handlers.SAMPLE_NOTE_URL = "https://gitcafe.com/zyxstar/md_note/raw/master/README.md"
 handlers.render = web.template.render(os.path.join(app_root,'static/templates'))
 
 web.config.debug = True
