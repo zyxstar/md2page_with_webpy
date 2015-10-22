9d4ef19a703a842a3cb4b6efd5866a4d66ac9196
zyxstar <248340019@qq.com>
2015-10-22 11:20:45 +0800

    Squashed 'static/' changes from 82f44c5..21b63ad
    21b63ad edit
    
    git-subtree-dir: static
    git-subtree-split: 21b63ad27f1adb747c6d1356972ebd49f3659a12

--- vendor/fake/fake.js
+++ vendor/fake/fake.js
@@ -1,12 +1,14 @@
-(function () {
+
+window.addEventListener('load', function(){
     var old = console.log;
     var logger = document.body;
-    console.log = function (message) {
-        old.call(null, message);
+    console.log = function(message) {
+        old.call(console, message);
         if (typeof message == 'object') {
             logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(message) : message) + '<br />';
         } else {
             logger.innerHTML += message + '<br />';
         }
     };
-})();
+}, false);
+
