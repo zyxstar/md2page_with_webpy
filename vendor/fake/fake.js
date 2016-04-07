c5fd416b1905a18a6ffffa0db0e6baf85cef4729
zyxstar <248340019@qq.com>
2016-04-07 14:11:38 +0800

    Squashed 'static/' changes from 9f55df8..3417c1c
    3417c1c edit
    24ae5b7 edit
    
    git-subtree-dir: static
    git-subtree-split: 3417c1cec255122dff9019b0ff7d02a3e32fcfa5

--- vendor/fake/fake.js
+++ vendor/fake/fake.js
@@ -1,18 +1,55 @@
 
-window.addEventListener('load', function(){
-    var old = console.log;
-    var logger = document.body;
+(function(){
+    var old_log = console.log;
+    var memo = [];
+
     console.log = function() {
         var args = Array.prototype.slice.call(arguments);
-        old.apply(console, args);
+        old_log.apply(console, args);
         if (args.length === 1){
-          args = args[0];
+            args = args[0];
         }
         if (typeof args == 'object') {
-            logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(args) : args) + '<br />';
+            memo.push((JSON && JSON.stringify ? JSON.stringify(args) : args) + '<br />');
         } else {
-            logger.innerHTML += args + '<br />';
+            memo.push(args + '<br />');
         }
+
+        retry_run(function(){
+            return get_logger();
+        }, function(){
+            while(memo.length)
+                get_logger().innerHTML += memo.splice(0,1)[0];
+        });
     };
-}, false);
+
+    function get_logger(){
+        return document.getElementById('logger');
+    }
+
+    function create_logger(){
+        var logger = document.createElement('DIV');
+        logger.id = 'logger';
+        document.body.appendChild(logger);
+    }
+
+    window.addEventListener('load', create_logger, false);
+
+})();
+
+
+function retry_run(fn_util_cond, fn_run_body, times, interval) {
+    var _times = times || 5;
+    var _interval = interval || 500;
+    (function () {
+        if (fn_util_cond()) {
+            fn_run_body();
+        } else if (_times > 0) {
+            _times--;
+            setTimeout(arguments.callee, _interval);
+        }
+    })();
+}
+
+
 
--- vendor/functional_study/functional_study.js
+++ vendor/functional_study/functional_study.js
@@ -42,3 +42,63 @@ function cat() {
 function construct(head, tail) {
   return cat([head], _.toArray(tail));
 }
+
+function project(table, keys) {
+  return _.map(table, function(obj) {
+    return _.pick.apply(null, construct(obj, keys));
+  });
+}
+
+function rename(obj, newNames) {
+  return _.reduce(newNames, function(o, nu, old) {
+    if (_.has(obj, old)) {
+      o[nu] = obj[old];
+      return o;
+    }
+    else
+      return o;
+  },
+  _.omit.apply(null, construct(obj, _.keys(newNames))));
+}
+
+function as(table, newNames) {
+  return _.map(table, function(obj) {
+    return rename(obj, newNames);
+  });
+}
+
+function restrict(table, pred) {
+  return _.reduce(table, function(newTable, obj) {
+    if (truthy(pred(obj)))
+      return newTable;
+    else
+      return _.without(newTable, obj);
+  }, table);
+}
+
+var globals = {};
+
+function makeBindFun(resolver) {
+  return function(k, v) {
+    var stack = globals[k] || [];
+    globals[k] = resolver(stack, v);
+    return globals;
+  };
+}
+
+var stackBinder = makeBindFun(function(stack, v) {
+  stack.push(v);
+  return stack;
+});
+
+var stackUnbinder = makeBindFun(function(stack) {
+  stack.pop();
+  return stack;
+});
+
+var dynamicLookup = function(k) {
+  var slot = globals[k] || [];
+  return _.last(slot);
+};
+
+
